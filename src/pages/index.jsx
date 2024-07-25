7b21952b239664e03b1288f5f0388c59ae5a0820
wangyd <54350138@qq.com>
2024-07-25 16:27:04 +0800

    feat: request请求

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { request } from 'umi';
 
 function PageIndex() {
   async function getGoods() {
@@ -19,12 +20,32 @@ function PageIndex() {
     console.log('🚀WYD-dev 🚝 handlerLogin 🚝 data:', data);
   }
 
+  async function requestGoods() {
+    const data = await request('/umi/goods');
+    console.log('🚀WYD-dev 🚝 requestGoods 🚝 data:', data);
+  }
+
+  async function requestLogin() {
+    const data = await request('/umi/login', {
+      method: 'POST',
+      data: {
+        username: 'admin',
+        password: '123456',
+      },
+    });
+    console.log('🚀WYD-dev 🚝 requestLogin 🚝 data:', data);
+  }
+
   return (
     <div>
       <h3>Page Index</h3>
       <h4>fetch</h4>
       <button onClick={getGoods}>获取goods数据</button>
       <button onClick={handlerLogin}>登录</button>
+
+      <h4>request</h4>
+      <button onClick={requestGoods}>获取goods数据</button>
+      <button onClick={requestLogin}>登录</button>
     </div>
   );
 }
